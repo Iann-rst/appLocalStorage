24109f53fac78135894e6be9b44bffa308421137
Iann-rst <iannrst11@gmail.com>
2022-04-11 20:37:16 -0300

    App

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens/Home';
+import { Form } from './src/screens/Form';
 
 export default function App() {
   return (
-    <Home />
+    <Form />
   )
 }
