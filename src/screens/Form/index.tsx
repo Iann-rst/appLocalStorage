4f256136d67b1e50becb2fc06367c4e2290fb3a9
Iann-rst <iannrst11@gmail.com>
2022-04-12 11:19:35 -0300

    Página Form (usando AsyncStorage)

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'react-native-uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Toast from 'react-native-toast-message';
+
+/*Styles*/
 import { Container, Content, ContainerForm, ContainerScrollView, Footer } from './styles';
 
+/*Componentes */
 import { HeaderForm } from '../../components/HeaderForm';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
 export function Form() {
+
+  const [name, setName] = useState("");
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  //função para pegar os dados dos inputs
+  async function handleNew() {
+    const id = uuid.v4();
+    const newData = {
+      id,
+      name,
+      user,
+      password,
+    }
+
+    //guardar no async storage, converte o objeto para texto com o JSON
+    await AsyncStorage.setItem("@savepass:passwords", JSON.stringify(newData));
+
+    Toast.show({
+      type: "success",
+      text1: "Cadastrado com sucesso!"
+    })
+
+    console.log(newData);
+  }
+
   return (
     <Container>
       <Content>
@@ -15,19 +48,23 @@ export function Form() {
           <ContainerForm>
             <Input
               label="Nome do serviço"
+              onChangeText={setName}
             />
             <Input
               label="E-mail ou usuário"
               autoCapitalize="none"
+              onChangeText={setUser}
             />
             <Input
               label="Senha"
               secureTextEntry
+              onChangeText={setPassword}
             />
           </ContainerForm>
 
           <Footer>
             <Button
+              onPress={handleNew}
               title="Salvar"
             />
           </Footer>
