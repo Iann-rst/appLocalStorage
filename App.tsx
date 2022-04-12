dbc740b7edd510f014cceb33e1a581809cd8a923
Iann-rst <iannrst11@gmail.com>
2022-04-12 11:19:51 -0300

    App (Toast message)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Toast from 'react-native-toast-message';
 import { Routes } from './src/routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <Toast />
+    </>
   )
 }
