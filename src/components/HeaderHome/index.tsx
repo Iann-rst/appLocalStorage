44feb5d00b940a1e7276686bfce040a90f71c9d0
Iann-rst <iannrst11@gmail.com>
2022-04-12 10:13:48 -0300

    Componente HeaderHome (botão add)

--- src/components/HeaderHome/index.tsx
+++ src/components/HeaderHome/index.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { Container, Imagem, ContainerUser, Title, Subtitle, ButtonVoltar } from './styles';
 import { MaterialIcons } from '@expo/vector-icons';
 
+import { useNavigation } from '@react-navigation/native';
+
 export function HeaderHome() {
+  const navigation = useNavigation();
+
+  function handleAdd() {
+    navigation.navigate("Form", {});
+  }
+
   return (
     <Container>
       <Imagem
@@ -13,13 +21,12 @@ export function HeaderHome() {
         <Subtitle>Sinta-se seguro aqui</Subtitle>
       </ContainerUser>
 
-      <ButtonVoltar>
+      <ButtonVoltar onPress={handleAdd}>
         <MaterialIcons
           name="add"
           size={22}
           color="#FFF"
         />
-
       </ButtonVoltar>
     </Container>
   )
