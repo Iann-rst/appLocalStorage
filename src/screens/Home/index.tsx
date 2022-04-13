d1df28771ce4e7279cd7911aa786d4546a753a25
Iann-rst <iannrst11@gmail.com>
2022-04-13 13:20:44 -0300

    Página Home (add: AsyncStorage)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,29 +1,50 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import { styles, Container, ContainerHeader, Texto, Footer } from './styles'
-import { Alert } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { styles } from './styles';
+import { Container, ContainerHeader, Texto, Footer } from './styles'
+
+import { FlatList } from 'react-native';
 
 import { Button } from '../../components/Button';
-
 import { HeaderHome } from '../../components/HeaderHome'
 import { Card, CardProps } from '../../components/Card';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FlatList } from 'react-native';
+
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 export function Home() {
   const [data, setData] = useState<CardProps[]>([]);
+  const { getItem, setItem } = useAsyncStorage("@savepass:passwords");
 
+
+  //Função para pegar os valores que estão no asyncStorage e colocar na FlatList
   async function handleFetchData() {
-    const response = await AsyncStorage.getAllKeys();
-    console.log(response)
+    /*Pega os valores {id, name, password, user} da coleção que salvos no asyncStorage */
+    const response = await getItem();
+    const data = response ? JSON.parse(response) : [];
+    console.log(data);
+    setData(data);
   }
 
-  useEffect(() => {
+  //função para deletar um item
+  async function handleRemove(id: string) {
+    const response = await getItem();
+    const previousData = response ? JSON.parse(response) : [];
+
+    //Faz um filtro nos dados que estão salvos e filtro por todos os registros menos o registro que tem o id que quero remover
+    const data = previousData.filter((item: CardProps) => item.id !== id)
+    //adiciono os dados no asyncStorage novamente (sem o item que foi deletado)
+    setItem(JSON.stringify(data));
+    //adiciona os dados para serem renderizados novamente na FlatList
+    setData(data);
+  }
+
+
+  /*Quando o focus voltar para a tela, ela é renderizada novamente. 
+  Atualizando e renderizando os novos dados*/
+  useFocusEffect(useCallback(() => {
     handleFetchData();
-  }, [])
+  }, []));
 
-  function handleClick() {
-    Alert.alert("Limpar a lista");
-  }
 
   return (
     <Container>
@@ -42,13 +63,13 @@ export function Home() {
         renderItem={({ item }) =>
           <Card
             data={item}
-            onPress={() => { }}
+            onPress={() => handleRemove(item.id)}
           />
         }
       />
 
       <Footer>
-        <Button onPress={handleClick} title="Limpar Lista" />
+        <Button title="Limpar Lista" />
       </Footer>
     </Container>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,44 +1,40 @@
 import styled from 'styled-components/native';
 import { StyleSheet } from 'react-native';
 
+export const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    width: '100%'
+  },
+  listContent: {
+    padding: 24,
+    paddingBottom: 150
+  }
+});
+
 export const Container = styled.View`
   flex:1;
-  background-color: #BDBDBD;
+  background-color: #F2F3F5;
+  align-items: center;
 `;
 
 export const ContainerHeader = styled.View`
 width: 100%;
-height: 300px;
-padding: 2px 24px;
 flex-direction: row;
 justify-content: space-between;
+align-items: center;
+margin-top: 10px;
+padding: 2px 24px;
 `;
 
 export const Texto = styled.Text`
   font-size: 20px;
   font-weight: bold;
-  color: #EFEFEF;
+  color: #3D434D;
 `;
 
 export const Footer = styled.View`
 width: 100%;
 padding: 24px;
 `;
-
-export const FlatList = styled.FlatList`
-flex:1;
-width: 100%;
-`;
-
-export const styles = StyleSheet.create({
-  list: {
-    flex: 1,
-    width: '100%'
-  },
-
-  listContent: {
-    padding: 24,
-    paddingBottom: 150
-  }
-})
 
